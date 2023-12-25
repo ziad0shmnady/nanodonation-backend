237c439b51959013362ead14afc5ea6250ca1478
Mostafa Gouda <mostafaegouda@gmail.com>
2023-12-26 00:22:13 +0200

    reafactor donate and add fields

--- src/payments/cardPointe/card-pointe.service.ts
+++ src/payments/cardPointe/card-pointe.service.ts
@@ -20,21 +20,22 @@ export class CardPointeService {
         source,
         type,
         duration,
+        frquency,
         donation_category_id,
         org_id,
         kiosk_id,
         token,
         expiry,
-        cronJob = false,
+        cvv
       } = req.body;
       // add data from the request to donation database
       const donation = await this.prismService.donation.create({
         data: {
           amount: parseFloat(amount),
           status: 'pending',
-          source: source || null,
-          type: cronJob ? 'recurring' : 'one_time',
-          duration: cronJob ? duration : null,
+          source: source,
+          type: type,
+          duration: `${frquency} ${duration}s`,
           donation_category_id: donation_category_id,
           org_id: org_id,
           user_id: req.user.userId,
@@ -47,36 +48,25 @@ export class CardPointeService {
         account: token,
         amount: amount,
         expiry: expiry,
+        cvv2: cvv,
         orderid: donation.donation_id,
       };
 
       const paymentRes = await this.cardpointeapi.post('/auth', data);
       console.log(paymentRes.data);
       // check if the payment is success
-      if (paymentRes.data.respstat !== 'A') {
-        await this.prismService.donation.update({
-          where: {
-            donation_id: donation.donation_id,
-          },
-          data: {
-            status: 'failed',
-            transaction_id: paymentRes.data.retref,
-          },
-        });
-      } else {
-        // if sucess update the status in the database
-        await this.prismService.donation.update({
-          where: {
-            donation_id: donation.donation_id,
-          },
-          data: {
-            status: 'success',
-            transaction_id: paymentRes.data.retref,
-          },
-        });
-      }
+      const paymentStatus = paymentRes.data.respstat === 'A' ? 'success' : 'failed';
+      await this.prismService.donation.update({
+        where: {
+          donation_id: donation.donation_id,
+        },
+        data: {
+          status:paymentStatus,
+          transaction_id: paymentRes.data.retref,
+        },
+      });
 
-      return res.status(400).send({ status: paymentRes.data.respstat });
+      return res.status(200).send({ status: paymentRes.data.respstat });
     } catch (error) {
       return res
         .status(HttpStatus.INTERNAL_SERVER_ERROR)
